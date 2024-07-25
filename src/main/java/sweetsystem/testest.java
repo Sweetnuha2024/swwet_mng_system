0e5c010decdb2a8e769e7b2b25acff140632d69b
shahd <s12114152@stu.najah.edu>
2024-07-25 23:01:33 +0300

    multy multy

--- src/main/java/sweetsystem/testest.java
+++ src/main/java/sweetsystem/testest.java
@@ -1,5 +1,18 @@
 package sweetsystem;
 
 public class testest {
+public int add(int x,int y) {
+	return x+y;
+}
 
+
+//hi shahd
+
+
+public int mul(int x,int y) {
+	return x*y;
+}
+public int sub(int x,int y) {
+	return x-y;
+}
 }
