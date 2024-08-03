a37fbd76e982e22c19fe54408260835534836c03
shahd <s12114152@stu.najah.edu>
2024-08-03 15:28:44 +0300

    update

--- src/test/java/sweetsystemmmm/Managepersonalaccountsteps.java
+++ src/test/java/sweetsystemmmm/Managepersonalaccountsteps.java
@@ -1,7 +1,67 @@
 package sweetsystemmmm;
 
+import Mysweetsystem2024.MyApplication;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class Managepersonalaccountsteps {
 
+	MyApplication app;
+	public Managepersonalaccountsteps(MyApplication ob) {
+		super();
+		this.app=ob;
+		
+	}
+	
+	
+	
+	@Given("the user is signed in to the Sweet System")
+	public void theUserIsSignedInToTheSweetSystem() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("the user adds their personal information with email {string} and phone {string}")
+	public void theUserAddsTheirPersonalInformationWithEmailAndPhone(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the information should be saved successfully")
+	public void theInformationShouldBeSavedSuccessfully() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("a confirmation message should be displayed")
+	public void aConfirmationMessageShouldBeDisplayed() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("the user updates their personal information with email {string} and phone {string}")
+	public void theUserUpdatesTheirPersonalInformationWithEmailAndPhone(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the changes should be saved successfully")
+	public void theChangesShouldBeSavedSuccessfully() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("the user deletes their personal information")
+	public void theUserDeletesTheirPersonalInformation() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the information should be removed successfully")
+	public void theInformationShouldBeRemovedSuccessfully() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
 	
 	
 	
@@ -9,11 +69,6 @@ public class Managepersonalaccountsteps {
 	
 	
 	
+	}
 	
-	
-	
-	
-	
-	
-	
-}
+	}
